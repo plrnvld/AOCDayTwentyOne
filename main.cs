612be232b049ca9c44261e2afec734ef4ea140cc
plrnvld <paulrijneveld@gmail.com>
2022-01-27 23:06:55 +0000

    Today

--- main.cs
+++ main.cs
@@ -1,23 +1,91 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Program 
 {
+    static int PLAYER_1_START = 10;
+    static int PLAYER_2_START = 9;
+
     public static void Main (string[] args) 
     {
-        var movesDict = new Dictionary<(int, int), List<(int, int)>>();
 
-        Console.WriteLine(PrevPos(4, 2)); // 2
-        Console.WriteLine(PrevPos(4, 3)); // 1
-        Console.WriteLine(PrevPos(4, 4)); // 10
-        Console.WriteLine(PrevPos(1, 9)); // 2
-        Console.WriteLine(PrevPos(1, 10)); // 1
-        Console.WriteLine(PrevPos(1, 20)); // 1
-        Console.WriteLine(PrevPos(1, 21)); // 10
-        Console.WriteLine(PrevPos(1, 11)); // 10
-        Console.WriteLine(PrevPos(1, 12)); // 9
+        var start = new Position(0, 0, PLAYER_1_START, PLAYER_2_START, false);
+        var movesDict = new Dictionary<(int, int, int, bool), List<Position>>();
+
+        var genPositions = new List<Position>();
+        genPositions.Add(start);
+
+
+        for (var gen = 0; gen < 5; gen++)
+        {
+
+
+        }
 
         Console.WriteLine("Fin");
+    }
+
+    static IEnumerable<(int, int)> GetReachableFrom((int, int) score)
+    {
+        return new List<(int, int)>();
+    }
+
+    static IEnumerable<Position> CreatePlayerOneEndPositions()
+    {
+        yield break;
+    }
+}
+
+class Position 
+{
+    public int Player1Score { get; }
+    public int Player2Score { get; }
+    public int BoardPosPlayer1 { get; }
+    public int BoardPosPlayer2 { get; }
+    public bool Player1Moved { get; }
+
+    readonly IEnumerable<int> allowedMoves = Enumerable.Range(1, 3);
+
+    public Position(int player1Score, int player2Score, int boardPos1, int boardPos2, bool player1moved)
+    {
+        Player1Score = player1Score;
+        Player2Score = player2Score;
+        BoardPosPlayer1 = boardPos1;
+        BoardPosPlayer2 = boardPos1;
+        Player1Moved = player1moved;
+    }
+
+    public (int, int, int, int, bool) Key => (Player1Score, Player2Score, BoardPosPlayer1, BoardPosPlayer2, Player1Moved);
+
+    public (int, int) Scores => (Player1Score, Player2Score);
+
+    public bool IsValid => Player1Score >= 0 && Player2Score >= 0;
+
+    public bool IsWinning => Player1Score >= 21 || Player2Score >= 21;
+
+    public IEnumerable<Position> CreatePreviousPositions()
+    {
+        foreach (var move in allowedMoves)
+        {
+            var previousPos = PrevPos(BoardPos, move);
+            var prevPlayer1Score = Player1Moved ? Player1Score - BoardPos : Player1Score;
+            var prevPlayer2Score = Player1Moved ? Player2Score : Player2Score - BoardPos;
+
+            yield return new Position(prevPlayer1Score, prevPlayer2Score, previousPos, !Player1Moved);          
+        }
+    }
+
+    public IEnumerable<Position> CreateNextPositions()
+    {
+        foreach (var move in allowedMoves)
+        {
+            var nextPos = NextPos(BoardPos, move);
+            var nextPlayer1Score = Player1Moved ? Player1Score : Player1Score + nextPos;
+            var nextPlayer2Score = Player1Moved ? Player2Score + nextPos : Player2Score;
+
+            yield return new Position(nextPlayer1Score, nextPlayer2Score, nextPos, !Player1Moved);          
+        }
     }
 
     static int NextPos(int currentPos, int add)
@@ -44,60 +112,5 @@ class Program
             return 1;
 
         return sum + (tooLow/10 + 1)*10;
-    }
-
-    static IEnumerable<(int, int)> GetReachableFrom((int, int) score)
-    {
-        return new List<(int, int)>();
-    }
-
-    static IEnumerable<Position> CreatePlayerOneEndPositions()
-    {
-        yield break;
-    }
-}
-
-class Position 
-{
-    public int Player1Score { get; }
-    public int Player2Score { get; }
-    public int BoardPos { get; }
-    public Position NextPosition { get; }
-    public bool Player1Moved { get; }
-
-    public Position(int player1Score, int player2Score, int boardPos, bool player1moved, Position nextPosition)
-    {
-        Player1Score = player1Score;
-        Player2Score = player2Score;
-        BoardPos = boardPos;
-        Player1Moved = player1moved;
-        NextPosition = nextPosition;
-    }
-
-    public Position(int player1Score, int player2Score, int boardPos, bool player1moved) 
-        : this(player1Score, player2Score, boardPos, player1moved, null)
-    {
-    }
-
-    public (int, int) Scores => (Player1Score, Player2Score);
-
-    public IEnumerable<Position> CreatePreviousPositions()
-    {
-        if (HasNext)
-        {
-            var next = NextPosition;
-
-        }
-        else
-        {
-
-
-        }
-
-        yield break;
-    }
-
-    bool HasNext => NextPosition != null;
-
-
+    } 
 }
